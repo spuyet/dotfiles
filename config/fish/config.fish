a976d3641876cc4ab62417eba176206cbc469832
Sebastien Puyet <sebastien@puyet.fr>
2019-10-16 20:30:17 +0200

    add ruby shims

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -2,6 +2,11 @@ if not contains $HOME/.bin $PATH
   set -x PATH $HOME/.bin $PATH
 end
 
+if not contains $HOME/.rbenv/shims $PATH
+  set -x PATH $HOME/.rbenv/shims $PATH
+end
+
+
 if not contains $HOME/.rbenv/bin $PATH
   set -x PATH $HOME/.rbenv/bin $PATH
 end
