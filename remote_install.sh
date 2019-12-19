1b1457012f7a341b784ce2a09a3f8329039609c7
Sebastien Puyet <sebastien@puyet.fr>
2019-12-20 00:04:40 +0100

    2 fast 2 furious

--- remote_install.sh
+++ remote_install.sh
@@ -1,6 +1,6 @@
 cd $HOME
-wget -q https://github.com/spuyet/dotfiles/archive/0.5.tar.gz
-tar -xzvf 0.5.tar.gz && rm 0.5.tar.gz
+wget -q https://github.com/spuyet/dotfiles/archive/0.6.tar.gz
+tar -xzvf 0.6.tar.gz && rm 0.6.tar.gz
 mv dotfiles-0.4 .dotfiles
 cd .dotfiles
 sh install.sh
