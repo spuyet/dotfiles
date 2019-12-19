19ee690d489af17accb42d390f5925f989d81659
Sebastien Puyet <sebastien@puyet.fr>
2019-12-20 00:02:47 +0100

    release 0.5 - so much work

--- remote_install.sh
+++ remote_install.sh
@@ -1,6 +1,6 @@
 cd $HOME
-wget -qO - https://github.com/spuyet/dotfiles/archive/0.4.tar.gz
-tar -xzvf 0.4.tar.gz && rm 0.4.tar.gz
+wget -q https://github.com/spuyet/dotfiles/archive/0.5.tar.gz
+tar -xzvf 0.5.tar.gz && rm 0.5.tar.gz
 mv dotfiles-0.4 .dotfiles
 cd .dotfiles
 sh install.sh
