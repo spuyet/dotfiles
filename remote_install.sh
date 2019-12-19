9a934914e61249c3029e34effc1e069cd42290a9
Sebastien Puyet <sebastien@puyet.fr>
2019-12-20 00:06:19 +0100

    faster is not better

--- remote_install.sh
+++ remote_install.sh
@@ -1,7 +1,7 @@
 cd $HOME
-wget -q https://github.com/spuyet/dotfiles/archive/0.6.tar.gz
-tar -xzvf 0.6.tar.gz && rm 0.6.tar.gz
-mv dotfiles-0.4 .dotfiles
+wget -q https://github.com/spuyet/dotfiles/archive/0.7.tar.gz
+tar -xzvf 0.7.tar.gz && rm 0.7.tar.gz
+mv dotfiles-0.7 .dotfiles
 cd .dotfiles
 sh install.sh
 cd -
