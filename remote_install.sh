f9ea956bab5377a3bf286d4cf383be01687529b2
Sebastien Puyet <sebastien@puyet.fr>
2019-12-19 23:56:52 +0100

    replace curl with wget

--- remote_install.sh
+++ remote_install.sh
@@ -1,5 +1,5 @@
 cd $HOME
-wget https://github.com/spuyet/dotfiles/archive/0.3.tar.gz
+wget -qO - https://github.com/spuyet/dotfiles/archive/0.3.tar.gz
 tar -xzvf 0.3.tar.gz && rm 0.3.tar.gz
 mv dotfiles-0.3 .dotfiles
 cd .dotfiles
