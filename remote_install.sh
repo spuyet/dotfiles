c36e0c4938c11ae7aa0260dc3ebc8dc2ae1a23a6
Sébastien Puyet <sebastien@puyet.fr>
2020-09-09 19:48:56 +0200

    Update remote_install.sh

--- remote_install.sh
+++ remote_install.sh
@@ -1,7 +1,7 @@
 cd $HOME
-wget -q https://github.com/spuyet/dotfiles/archive/0.10.tar.gz
-tar -xzvf 0.10.tar.gz && rm 0.10.tar.gz
-mv dotfiles-0.10 .dotfiles
+wget -q https://github.com/spuyet/dotfiles/archive/0.11.tar.gz
+tar -xzvf 0.11.tar.gz && rm 0.11.tar.gz
+mv dotfiles-0.11 .dotfiles
 cd .dotfiles
 sh install.sh
 cd -
