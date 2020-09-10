b670901fa78a596d4c75cd1a1581b1c6d075d45d
Sébastien Puyet <sebastien@puyet.fr>
2020-09-10 10:09:35 +0200

    Update install_linux_packages.sh

--- install_linux_packages.sh
+++ install_linux_packages.sh
@@ -11,7 +11,7 @@ done
 sudo apt-add-repository ppa:fish-shell/release-3 -y
 sudo apt update -y
 
-pkglist="libjemalloc-dev git fish fonts-powerline tmux curl gcc make htop libssl-dev libreadline-dev zlib1g-dev"
+pkglist="libjemalloc-dev git fish fonts-powerline tmux curl gcc make htop libssl-dev libreadline-dev zlib1g-dev vim"
 
 sudo apt install -y $pkglist
 
